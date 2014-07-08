e685da68dc82a3a44af7106c2513f0bf327fbc12
brownnrl <brownnrl@gmail.com>
2014-07-08 17:22:08 -0400

    Some changes to sage cell client.

--- pelicansage/sagecell-client.py
+++ pelicansage/sagecell-client.py
@@ -9,48 +9,59 @@ import threading
 import json
 import urllib.request
 import shutil
+from uuid import uuid4
 
 class SageCell(object):
     def __init__(self, url, timeout=10):
+
         if not url.endswith('/'):
             url+='/'
+        self.url = url
+        self.timeout = timeout
+
         # POST or GET <url>/kernel
         # if there is a terms of service agreement, you need to
         # indicate acceptance in the data parameter below (see the API docs)
-        req = urllib.request.Request(url=url+'kernel', data=bytes('accepted_tos=true','utf-8'),headers={'Accept': 'application/json'})
-        resp = urllib.request.urlopen(req).read().decode('utf8')
-        response = json.loads(resp)
+        self.req = urllib.request.Request(url=url+'kernel', data=bytes('accepted_tos=true','utf-8'),headers={'Accept': 'application/json'})
 
-        # RESPONSE: {"id": "ce20fada-f757-45e5-92fa-05e952dd9c87", "ws_url": "ws://localhost:8888/"}
-        # construct the iopub and shell websocket channel urls from that
+        self._message_ids = []
 
-        self.kernel_url = response['ws_url']+'kernel/'+response['id']+'/'
-        websocket.setdefaulttimeout(timeout)
-        print("\n\n!! KERNEL !!\n\n" + self.kernel_url)
-        self._shell = websocket.create_connection(self.kernel_url+'shell')
-        self._iopub = websocket.create_connection(self.kernel_url+'iopub')
+        self._running = False
 
-        # initialize our list of messages
-        self.shell_messages = []
-        self.iopub_messages = []
+    def _begin_requests(self):
+
+        if not self._running:
+            resp = urllib.request.urlopen(self.req).read().decode('utf8')
+            response = json.loads(resp)
+
+            # RESPONSE: {"id": "ce20fada-f757-45e5-92fa-05e952dd9c87", "ws_url": "ws://localhost:8888/"}
+            # construct the iopub and shell websocket channel urls from that
+
+            self.kernel_url = response['ws_url']+'kernel/'+response['id']+'/'
+            websocket.setdefaulttimeout(self.timeout)
+            self._shell = websocket.create_connection(self.kernel_url+'shell')
+            self._iopub = websocket.create_connection(self.kernel_url+'iopub')
+
+            # initialize our list of messages
+            self.shell_messages = []
+            self.iopub_messages = []
+
+            # We use threads so that we can simultaneously get the messages on both channels.
+            self.threads = [threading.Thread(target=self._get_iopub_messages), 
+                            threading.Thread(target=self._get_shell_messages)]
+            for t in self.threads:
+                t.start()
 
     def execute_request(self, code):
         # zero out our list of messages, in case this is not the first request
-        self.shell_messages = []
-        self.iopub_messages = []
-
-        # We use threads so that we can simultaneously get the messages on both channels.
-        threads = [threading.Thread(target=self._get_iopub_messages), 
-                    threading.Thread(target=self._get_shell_messages)]
-        for t in threads:
-            t.start()
+        self._begin_requests()
 
         # Send the JSON execute_request message string down the shell channel
         msg = self._make_execute_request(code)
         self._shell.send(msg)
 
         # Wait until we get both a kernel status idle message and an execute_reply message
-        for t in threads:
+        for t in self.threads:
             t.join()
 
         return {'kernel_url': self.kernel_url, 'shell': self.shell_messages, 'iopub': self.iopub_messages}
@@ -72,12 +83,14 @@ class SageCell(object):
                 break
 
     def _make_execute_request(self, code):
-        from uuid import uuid4
-        import json
-        session = str(uuid4())
+        if not self._running:
+            self.session = str(uuid4())
+
+        message = str(uuid4())
+        self._message_ids.append(message)
 
         # Here is the general form for an execute_request message
-        execute_request = {'header': {'msg_type': 'execute_request', 'msg_id': str(uuid4()), 'username': '', 'session': session},
+        execute_request = {'header': {'msg_type': 'execute_request', 'msg_id': message, 'username': '', 'session': self.session},
                             'parent_header':{},
                             'metadata': {},
                             'content': {'code': code, 'silent': False, 'user_variables': [], 'user_expressions': {'_sagecell_files': 'sys._sage_.new_files()'}, 'allow_stdin': False}}
