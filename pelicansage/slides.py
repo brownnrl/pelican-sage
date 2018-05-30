c0f493b246348a33a9e8f9f3ea0eecee54296006
brownnrl <brownnrl@gmail.com>
2018-05-29 20:49:13 -0400

    (#10) Support Slides
    Changed to do a simple pass through to hovercraft generate function,
    recreating the argument parser for each file.

--- pelicansage/slides.py
+++ pelicansage/slides.py
@@ -1,13 +1,8 @@
 from pelican.generators import CachingGenerator
 from pelican.contents import Content
 
-from io import StringIO
-
-from itertools import chain
-
-from hovercraft.parse import HovercraftReader, rst2xml, Writer
-from hovercraft.generate import rst2html
-from hovercraft.template import Template
+from hovercraft.generate import generate
+from argparse import Namespace
 import os
 
 import logging
@@ -34,59 +29,34 @@ class SlidesGenerator(CachingGenerator):
         super(SlidesGenerator, self).__init__(*args, **kwargs)
 
     def generate_context(self):
-        all_slides = []
-        hidden_slides = []
+        pass
 
-        logger.debug("CALLING SLIDES GENERATOR CONTEXT")
+    def generate_output(self, writer):
+
+        template_path = os.path.abspath(self.settings['SLIDES_THEME'])
+        output_path = os.path.abspath(os.path.join(self.output_path, 'slides'))
+        mathjax_cdn = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML'
 
         for f in self.get_files(
                 self.settings['SLIDE_PATHS'],
                 exclude=self.settings['SLIDE_EXCLUDES']):
-            slide_page = self.get_cached_data(f, None)
-            if slide_page is None:
-                try:
-                    slide_page, _ = rst2html(os.path.join(self.path, f), Template())
-                    slide_page = slide_page.decode("UTF-8")
-                    """
-                    slide_page = self.readers.read_file(
-                        base_path=self.path,
-                        path=f,
-                        content_class=Slides,
-                        context=self.context,
-                        preread_signal=None,
-                        context_signal=None,
-                        context_sender=None)
-                    """
-                except Exception as e:
-                    logger.error(
-                        'Could not process %s\n%s',
-                        f, e, exec_info=self.settings.get('DEBUG', False))
-                    self._add_failed_source_path(f)
 
-            all_slides.append(slide_page)
-            """
-            if not slide_page.is_valid():
+            presentation = os.path.abspath(os.path.join('content', f))
+            args = Namespace(presentation=presentation,
+                             template=template_path,
+                             targetdir=output_path,
+                             css=None,
+                             js=None,
+                             auto_console=False,
+                             slide_numbers=True,
+                             skip_help=False,
+                             skip_notes=False,
+                             mathjax=os.environ.get('HOVERCRAFT_MATHJAX', mathjax_cdn))
+
+
+            try:
+                generate(args)
+            except Exception as e:
+                logger.error(
+                    'Could not process %s\n%s', f, e)
                 self._add_failed_source_path(f)
-                continue
-
-            if slide_page.status == "published":
-                all_slides.append(slide_page)
-            elif slide_page.status == "hidden":
-                hidden_slides.append(slide_page)
-            self.add_source_path(slide_page)
-            self._update_context(('slides', 'hidden_slides'))
-            self.save_cache()
-            self.readers.save_cache()
-            """
-
-        self.slides = all_slides
-        self.hidden_slides = hidden_slides
-
-    def generate_output(self, writer):
-        default_template = Template()
-        logger.error("CALLING GENERATE OUTPUT")
-
-        for idx, slides in enumerate(chain(self.slides, self.hidden_slides)):
-            with open("contentslides%s.html" % (idx,), "w") as f:
-                f.write(slides)
-
